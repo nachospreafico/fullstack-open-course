f5dbba91bfc23a7b7fb5d22eb55ab91d8aa40f4a
Ignacio Spreafico <nachospreafico06@gmail.com>
2024-01-19 13:34:03 +0100

    finished up to and including exercises 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,21 +1,62 @@
 import "./App.css";
 
-function Hello({ name }) {
-  return <p>Hello, {name}!</p>;
-}
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
 
-function App() {
-  const a = 1;
-  const b = 3;
+const Content = ({ parts }) => {
+  return (
+    <>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <p>
-        {a} + {b} = {a + b}
-      </p>
-      <Hello name="Nacho" />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
-}
+};
 
 export default App;
